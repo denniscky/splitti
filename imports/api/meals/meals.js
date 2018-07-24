4b4f31a299c5c0b5eae3f017f42155e56e0316dd
Dennis Chan <dchan@dmcouncil.org>
2018-07-24 15:39:34 -0400

    Basic home page modified;

--- imports/api/meals/meals.js
+++ imports/api/meals/meals.js
@@ -7,15 +7,16 @@ class MealsCollection extends Mongo.Collection {
 export const Meals = new MealsCollection('meals');
 
 // Deny all client-side updates since we will be using methods to manage this collection
-Meals.deny({
-  insert() { return true; },
-  update() { return true; },
-  remove() { return true; },
-});
+// Meals.deny({
+//   insert() { return true; },
+//   update() { return true; },
+//   remove() { return true; },
+// });
 
 Meals.schema = new SimpleSchema({
-  _id: { type: String, regEx: SimpleSchema.RegEx.Id },
-  name: { type: String }
+  code: { type: String },
+  tax: { type: Number },
+  tip: { type: Number },
 });
 
 Meals.attachSchema(Meals.schema);
